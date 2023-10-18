4f5874515e677de2c88385ff4935c56b311654b8
Leen-odeh3 <123558998+Leen-odeh3@users.noreply.github.com>
2023-10-18 13:25:31 +0300

    First class and method ToString()

---
+++ C#/OOP-Csharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+
+            for (int i = 0; i < total; i++)
+            {
+                persons[i] = new Person()
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+    }
+
+
+}
